1c45376cfdae69845ec938fdba03faf59ec1b223
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-02-17 00:18:48 +0000

    Change Scene

---
+++ Project/src/Projects/ChangeScene/ChangeScene.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_BackBt_pressed():
+	get_tree().change_scene("res://src/Scenes/Start.tscn");
+	pass # Replace with function body.
---
+++ Project/src/Scenes/Start.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_ChangeSceneBtn_pressed():
+	get_tree().change_scene("res://src/Projects/ChangeScene/ChangeScene.tscn");
+	pass # Replace with function body.
