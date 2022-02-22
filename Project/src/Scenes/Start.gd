b82681f3ba85f8069c1796e481648698f1dc57b1
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-02-22 00:28:39 +0000

    Organize UI

--- Project/src/Scenes/Start.gd
+++ Project/src/Scenes/Start.gd
@@ -4,11 +4,9 @@ extends Control
 func _ready():
 	pass
 
-
 func _on_ChangeSceneBtn_pressed():
 	get_tree().change_scene("res://src/Projects/ChangeScene/ChangeScene.tscn");
 	pass # Replace with function body.
-
 
 func _on_RaioXBtn_pressed():
 	get_tree().change_scene("res://src/Projects/Raio-X/RaioX.tscn");
